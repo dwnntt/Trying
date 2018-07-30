d25fd4fc6757687675c29b6d89100a565907bcf1
Suci <sucidew98@gmail.com>
2018-07-30 11:27:44 +0700

    add new file lat44

---
+++ lat44.py
@@ -0,0 +1,7 @@
+import getpass
+
+password = getpass.getpass()
+print 'Password anda :', password
+
+password = getpass.getpass(prompt='Inputkan password anda :')
+print 'Password anda : ', password
